20b51515752dde5405ab10b3a37ee27affe9dc9f
Nat Wilson <njwilson23@gmail.com>
2014-03-08 21:14:26 -0500

    RK4 should return f', not f (timestep unknown)

--- lib/ode.py
+++ lib/ode.py
@@ -1,13 +1,21 @@
 
+from typecheck import verifytypes
+
+@verifytypes
 def rk4step(f, x, y, h):
     """ Perform a single step of Runge-Kutta 4 integration for either a single
     equation or a system of equations.
 
-    <f::Function, x::Float[], y::Float[], h::Float>
+    *f* should be a function y' = f(x,y)
+    x is the independent variable
+    y is the dependent variable (the solution space)
+    h is the space step
+
+    <f::function, x::float[], y::float[], h::float>
     """
     k1 = h*f(x, y)
     k2 = h*f(x+0.5*h, y+0.5*k1)
     k3 = h*f(x+0.5*h, y+0.5*k2)
     k4 = h*f(x+h, y+k3)
-    return y + (k1 + 2*k2 + 2*k3 + k4)/6.0
+    return (k1 + 2*k2 + 2*k3 + k4)/6.0
 
