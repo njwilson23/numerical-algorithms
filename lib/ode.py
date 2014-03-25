13eddf508796045e93893401ccc5b70f4e6ee00c
Nat Wilson <njwilson23@gmail.com>
2014-03-25 11:40:47 -0400

    add TVD RK3 integrator

--- lib/ode.py
+++ lib/ode.py
@@ -9,7 +9,7 @@ def rk4step(f, x, y, h):
     *f* should be a function y' = f(x,y)
     x is the independent variable
     y is the dependent variable (the solution space)
-    h is the space step
+    h is the space/time step
 
     <f::function, x::float[], y::float[], h::float>
     """
@@ -18,4 +18,18 @@ def rk4step(f, x, y, h):
     k3 = h*f(x+0.5*h, y+0.5*k2)
     k4 = h*f(x+h, y+k3)
     return (k1 + 2*k2 + 2*k3 + k4)/6.0
+
+@verifytypes
+def tvdrk3step(f, u, h):
+    """ Strong stability-preserving Runge-Kutta 3 integration.
+
+    *f* should be a function y' = f(u)
+    u is the dependent variable (the solution space)
+    h is the space/time step
+
+    <f::function, u::float[], h::float>
+    """
+    u1 = u + h*f(u)
+    u2 = 0.75*u + 0.25*(u1 + h*f(u1))
+    return (u/3.0) + 2.0/3.0 * (u2 + h*f(u2))
 
