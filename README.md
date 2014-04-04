6698afcd5236504bbef08809f869bc7958d85f31
Nat Wilson <njwilson23@gmail.com>
2014-04-04 11:52:22 -0400

    add gauss seidel and weight jacobi to solvers
    refactored solvers to make more efficient and make adding new ones easier

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# numerical-algorithms
+# Numerical Algorithms
 
-Collection of basic numerical algorithms written in Python, intended primarily
-for education.
+Collection of basic numerical algorithms written in Python and Julia, intended
+for education. The codes contained here are generally proof-of-concept quality.
+
+Contents
+--------
+- Root-finding
+- Runge-Kutta ODE solvers
+- Discrete finite difference operators
+- Fast Poisson solvers
+- Example PDE solvers for
+    - Scalar wave equation
+    - Convection-diffusion
+    - Navier-Stokes
+- Iterative linear system solvers
+    - Jacobi
+    - Gauss-Seidel
 
--- lib/la/IterativeSolvers.jl
+++ lib/la/IterativeSolvers.jl
@@ -1,27 +1,46 @@
-module Jacobi
+module IterativeSolvers
 
-# Jacobi iteration:
-#
-# Iteratively approximate Ax=b as
-# Px = (P-A)x + b
-# where P = diag(A)
-function jacobi_iter(A::AbstractArray, x0::Array, b::Array)
-    P = spdiagm(diag(A), 0)
-    return P\((P-A)*xk + b)
+function solve_iter(P::AbstractArray, A::AbstractArray, x::Array, b::Array, n::Int)
+    for i=1:n
+        x = P \ ((P-A)*x + b)
+    end
+    return x
 end
 
-n = 16
+# Jacobi iteration
+# Iteratively approximate Ax=b as
+# Px = (P-A)x + b
+# where P = diag(A).
+# Optionally, performs weighted Jacobi, where P = diag(A)/w
+function jacobi(A, x, b, n; w=1.0)
+    P = spdiagm(diag(A) / w, 0)
+    x = solve_iter(P, A, x, b, n)
+    return x
+end
+
+weightedjacobi(A, x, b, n) = jacobi(A, x, b, n, w=2.0/3.0)
+
+# Gauss-Seidel iteration
+# Preconditioner P = diag(A) + tril(A)
+function gaussseidel(A, x, b, n)
+    P = tril(A)
+    x = solve_iter(P, A, x, b, n)
+    return x
+end
+
+
+# Test
+n = 32
 A = spdiagm((-ones(n-1), 2ones(n), -ones(n-1)), (-1, 0, 1))
 b = zeros(n)
 b[n/4:3n/4] = 1.0
+b = sin(linspace(0, 2pi, n))
+x_direct = A\b
 
-println(A\b)
-
-xk = ones(n)
-for i=1:200
-    xk = jacobi_iter(A, xk, b)
-end
-println(xk)
+x0 = randn(n)
+x_iter = gaussseidel(A, x0, b, 200)
+err = maximum(x_direct - x_iter)
+@printf("error: %2.3e\n", err)
 
 end #module
 
